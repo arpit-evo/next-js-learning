62f54e04b6c1fddc67fb35fe9f8899b0fa30c288
arpit-evo <arpit.m@evolutioncloud.in>
2024-06-26 16:02:37 +0530

    feat: add wavy backgroound

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <TestimonialCards />
       <UpcomingWebinar />
+      <Instructors />
     </main>
   );
 }
